3e896d1c1bb94536b7c0e48ded898de3ba3a16d6
Hanna Bennett <bennetthanna22@gmail.com>
2019-05-14 19:37:53 -0700

    Add Organization MVC framework

---
+++ db/migrate/20190515023530_create_organizations.rb
@@ -0,0 +1,15 @@
+class CreateOrganizations < ActiveRecord::Migration[5.2]
+  def change
+    create_table :organizations do |t|
+      t.string :name
+      t.string :location
+      t.string :ages_served
+      t.string :programs
+      t.string :contact_number
+      t.string :contact_email
+      t.string :website
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_29_090110) do
+ActiveRecord::Schema.define(version: 2019_05_15_023530) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "organizations", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.string "ages_served"
+    t.string "programs"
+    t.string "contact_number"
+    t.string "contact_email"
+    t.string "website"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
