ad7a054660f5a32246662712f477f2ffc894f880
Hanna Bennett <bennetthanna22@gmail.com>
2019-05-17 10:29:10 -0700

    Allow anyone to create activities and set state to :active if admin else :pending

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,7 +1,7 @@
 class ActivitiesController < ApplicationController
     before_action :set_activity, only: [:show, :edit, :update, :destroy]
-    skip_before_action :authenticate_user!, only: [:index, :show]
-    before_action :restrict_unless_admin, except: [:index, :show]
+    skip_before_action :authenticate_user!, only: [:index, :show, :new, :create]
+    before_action :restrict_unless_admin, except: [:index, :show, :new, :create]
   
     def index
       @tags = ActsAsTaggableOn::Tag.all
@@ -24,6 +24,8 @@ class ActivitiesController < ApplicationController
   
     def create
       @activity = Activity.new(activity_params)
+      @activity.state = current_user&.admin? ? :active : :pending
+
       if @activity.save
         redirect_to activities_path
         flash[:success] = 'Activity was successfully created.'
