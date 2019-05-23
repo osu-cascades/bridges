4ed705830753c7ffb2f7e3c94c943e31e2df3b15
Hanna Bennett <bennetthanna22@gmail.com>
2019-05-22 23:23:47 -0700

    Link Vue activity cards to get and edit respective activity

--- app/javascript/components/activity_card.vue
+++ app/javascript/components/activity_card.vue
@@ -1,13 +1,19 @@
 <template>
-  <container>
-    <div class="col-lg-4 center" v-for="activity in activities">
-      <div class="col-lg-12 center">
-        <h3>{{ activity.title }}</h3>
-        <p>Edit</p>
-        <button class="btn btn-secondary btn-sm" v-for="tag in activity.tag_list">{{ tag }}</button>
+  <div class="activity-card">
+    <div v-for="activity in activities">
+      <div class="col-lg-4 center">
+        <div class="col-lg-12 center">
+          <h3>
+            <a v-on:click="select(activity.id)" v-bind:href="`/activities/${activity.id}`">{{ activity.title }}</a>
+          </h3>
+          <p>
+            <a v-bind:href="`/activities/${activity.id}/edit`">Edit</a>
+          </p>
+          <div class="btn btn-secondary btn-sm btn-static" v-for="tag in activity.tag_list">{{ tag }}</div>
+        </div>
       </div>
     </div>
-  </container>
+  </div>
 </template>
 
 <script>
@@ -17,18 +23,6 @@ export default {
       type: Array,
       required: true
     }
-  },
-  methods: {
-    select: function(id) {
-      console.log('selecting id', id);
-    },
-    edit: function(id) {
-      console.log('editing id', id);
-    }
   }
 }
 </script>
-
-<style lang="scss" scoped>
-
-</style>
