75b734fb5fdd15b96e275c8a05bcd48e1091ef3b
Hanna Bennett <bennetthanna22@gmail.com>
2019-05-29 23:19:17 -0700

    Fix activities dashboard layout to be in rows of 3

--- app/javascript/components/activity_card.vue
+++ app/javascript/components/activity_card.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="activity-card">
-    <div v-for="activity in activitiesData">
-      <div class="col-lg-4 center">
-        <div class="col-lg-12 center" v-bind:class="activityState">
-          <h3>
-            <a v-on:click="select(activity.id)" v-bind:href="`/activities/${activity.id}`">{{ activity.title }}</a>
-          </h3>
-          <p>
-            <a v-bind:href="`/activities/${activity.id}/edit`">Edit</a>
-          </p>
-          <div class="btn btn-secondary btn-sm btn-static" v-for="tag in activity.tag_list">{{ tag }}</div>
+    <div v-for="activityGroup in activitiesData">
+      <div class="row display-flex">
+        <div v-for="activity in activityGroup" class="col-lg-4 center">
+          <div class="col-lg-12 center" v-bind:class="activityState">
+            <h3>
+              <a v-on:click="select(activity.id)" v-bind:href="`/activities/${activity.id}`">{{ activity.title }}</a>
+            </h3>
+            <p>
+              <a v-bind:href="`/activities/${activity.id}/edit`">Edit</a>
+            </p>
+            <div class="btn btn-secondary btn-sm btn-static" v-for="tag in activity.tag_list">{{ tag }}</div>
+          </div>
         </div>
       </div>
     </div>
@@ -17,6 +19,7 @@
 </template>
 
 <script>
+import _ from 'lodash';
 import { eventBus } from '../packs/activities';
 export default {
   props: {
@@ -30,12 +33,12 @@ export default {
   },
   data: function () {
     return {
-      activitiesData: this.activities
+      activitiesData: _.chunk(this.activities, 3)
     }
   },
   created: function () {
     eventBus.$on(`${this.activityState}_activities_updated`, activities => {
-      this.activitiesData = activities
+      this.activitiesData = _.chunk(activities, 3)
     });
   }
 }
