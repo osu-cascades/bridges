ada969862da2b048fa7265190005b771ecde1440
Hanna Bennett <bennetthanna22@gmail.com>
2019-07-09 21:53:39 -0700

    Update Activity views with altered attributes References #7

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -85,7 +85,7 @@ class ActivitiesController < ApplicationController
     def activity_params
       params[:activity][:when_start] = Time.strptime(params[:activity][:when_start], '%m/%d/%Y %H:%M') if !params[:activity][:when_start].blank?
       params[:activity][:when_end] = Time.strptime(params[:activity][:when_end], '%m/%d/%Y %H:%M') if !params[:activity][:when_end].blank?
-      params.require(:activity).permit(:title,:who,:what,:where,:when_start,:when_end,:how,:details,:tag_list,:state)
+      params.require(:activity).permit(:title,:when_start,:when_end,:description,:location,:contact_name,:contact_number,:contact_email,:state,:tag_list)
     end
   end
   
