291e730e169835845dd198fe00f7455642fadf02
Hanna Bennett <bennetthanna22@gmail.com>
2019-07-09 22:03:56 -0700

    Add guest user to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,14 @@ users = [
     password: "password",
     password_confirmation: "password",
     role: "admin"
+  },
+  {
+    first_name:  "User",
+    last_name: "Example",
+    email: "user@example.com",
+    password: "password",
+    password_confirmation: "password",
+    role: "guest"
   }
 ]
 
