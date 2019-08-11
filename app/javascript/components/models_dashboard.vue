4f7ae0697279703ef3d3f1e69d06f018f9d4d3a9
Hanna Bennett <bennetthanna22@gmail.com>
2019-08-10 23:13:15 -0700

    Refactor dashboard template to reduce duplicate code

--- app/javascript/components/models_dashboard.vue
+++ app/javascript/components/models_dashboard.vue
@@ -5,23 +5,13 @@
       <a class="btn btn-primary" v-if="admin || modelSingular === 'activity'" v-bind:title="`Add a new ${modelSingular}`" v-bind:href="`/${modelPlural}/new`">Add {{ modelSingular }}</a>
       <button v-on:click="toggleView" title="Toggle view" class="btn btn-primary">View as {{ view === 'card' ? 'list' : 'cards' }}</button>
     </nav>
-    <div class="container" v-if="admin && view === 'card'">
-      <ModelCard :modelDisplayAttributes="modelDisplayAttributes" :modelDisplayTitle="modelDisplayTitle" :modelPlural="modelPlural" modelState="pending" :models="pendingModelsData" :admin="admin"></ModelCard>
-    </div>
-    <div class="container" v-if="admin && view === 'list'">
-      <ModelList :modelDisplayTitle="modelDisplayTitle" :modelPlural="modelPlural" modelState="pending" :models="pendingModelsData" :admin="admin"></ModelList>
-    </div>
-    <div class="container" v-if="view === 'card'">
-      <ModelCard :modelDisplayAttributes="modelDisplayAttributes" :modelDisplayTitle="modelDisplayTitle" :modelPlural="modelPlural" modelState="active" :models="modelsData" :admin="admin"></ModelCard>
-    </div>
-    <div class="container" v-if="view === 'list'">
-      <ModelList :modelDisplayTitle="modelDisplayTitle" :modelPlural="modelPlural" modelState="active" :models="modelsData" :admin="admin"></ModelList>
-    </div>
-    <div class="container" v-if="admin && view === 'card'">
-      <ModelCard :modelDisplayAttributes="modelDisplayAttributes" :modelDisplayTitle="modelDisplayTitle" :modelPlural="modelPlural" modelState="denied" :models="deniedModelsData" :admin="admin"></ModelCard>
-    </div>
-    <div class="container" v-if="admin && view === 'list'">
-      <ModelList :modelDisplayTitle="modelDisplayTitle" :modelPlural="modelPlural" modelState="denied" :models="deniedModelsData" :admin="admin"></ModelList>
+    <div v-for="state in modelStates">
+      <div class="container" v-if="admin && view === 'card'">
+        <ModelCard :modelDisplayAttributes="modelDisplayAttributes" :modelDisplayTitle="modelDisplayTitle" :modelPlural="modelPlural" :modelState="state" :models="bindData(state)" :admin="admin"></ModelCard>
+      </div>
+      <div class="container" v-if="admin && view === 'list'">
+        <ModelList :modelDisplayTitle="modelDisplayTitle" :modelPlural="modelPlural" :modelState="state" :models="bindData(state)" :admin="admin"></ModelList>
+      </div>
     </div>
   </div>
 </template>
@@ -72,8 +62,9 @@ export default {
   },
   data: function () {
     return {
+      modelStates: ['pending', 'active', 'denied'],
       tagsData: this.tags,
-      modelsData: this.models,
+      activeModelsData: this.models,
       pendingModelsData: this.pendingModels,
       deniedModelsData: this.deniedModels,
       view: 'card'
@@ -82,11 +73,14 @@ export default {
   methods: {
     toggleView: function () {
       this.view = this.view === 'card' ? 'list' : 'card';
+    },
+    bindData: function(state) {
+      return this[`${state}ModelsData`];
     }
   },
   created: function () {
     eventBus.$on(`active_${this.modelPlural}_updated`, models => {
-      this.modelsData = models;
+      this.activeModelsData = models;
     });
   },
   components: {
