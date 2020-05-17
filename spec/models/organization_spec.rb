d8d51e43708687e98f344d79034fdb42e566d622
Yong Joseph Bakos <ybakos@humanoriented.com>
2020-05-17 11:09:32 -0700

    Organization: Test attributes and validators.

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -3,6 +3,21 @@ require 'rails_helper'
 RSpec.describe Organization, type: :model do
   let(:organization) { build(:organization) }
 
+  describe 'attributes' do
+    specify { expect(organization).to respond_to(:name) }
+    specify { expect(organization).to respond_to(:location) }
+    specify { expect(organization).to respond_to(:ages_served) }
+    specify { expect(organization).to respond_to(:programs) }
+    specify { expect(organization).to respond_to(:contact_number) }
+    specify { expect(organization).to respond_to(:contact_email) }
+    specify { expect(organization).to respond_to(:website) }
+    specify { expect(organization).to respond_to(:description) }
+  end
+
+  describe 'validations' do
+    specify { expect(organization).to validate_presence_of(:name) }
+  end
+
   describe 'associations' do
     specify { expect(organization).to have_many(:users) }
   end
