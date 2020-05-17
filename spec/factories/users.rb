7d497c0f6d01bac10b37392c34971a1c3a6c8aee
Yong Joseph Bakos <ybakos@humanoriented.com>
2020-05-17 10:24:03 -0700

    User: Massage factory and refactor tests.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,16 +1,14 @@
 FactoryBot.define do
-  sequence :email do |n|
-    "example#{n}@example.com"
-  end
 
   factory :user do
-    first_name { 'Bob' }
-    last_name { 'Builder' }
-    email
+    first_name { 'Fake' }
+    last_name { 'User' }
+    sequence(:email) { |n| "fake_#{n}@fake.com" }
     password { 'password' }
     password_confirmation { 'password' }
 
     trait :admin do
+      last_name { 'Admin' }
       role { 'admin' }
     end
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,47 +18,52 @@ RSpec.describe User, type: :model do
     specify { expect(user).to validate_presence_of(:last_name) }
   end
 
-  it 'has a string representation of first_name last_name' do
-    expect(user.to_s).to eq('Bob Builder')
-  end
-
-  it 'has a string representation of last_name, first_name' do
-    expect(user.last_name_first_name).to eq('Builder, Bob')
-  end
-
-  it 'retains the original full name during a name change' do
-    original_full_name = "#{user.first_name} #{user.last_name}"
-    user.first_name = "CHANGED"
-    expect(original_full_name).to eq(user.name_was)
-  end
-
   it 'has a default role of guest' do
     new_user = User.new
     expect(new_user.role).to eq('guest')
   end
 
   it 'is active by default' do
-    new_user = User.new
-    expect(new_user.active?).to eq(true)
+    expect(User.new.active?).to eq(true)
   end
 
-  it 'active user is active_for_authentication' do
-    new_user = User.new
-    new_user.active = true
-    expect(new_user.active_for_authentication?).to eq(true)
+  describe 'string representations' do
+    it 'has a string representation of first_name last_name' do
+      result = "#{user.first_name} #{user.last_name}"
+      expect(user.to_s).to eq(result)
+    end
+    it 'has a string representation of last_name, first_name' do
+      result = "#{user.last_name}, #{user.first_name}"
+      expect(user.last_name_first_name).to eq(result)
+    end
   end
 
-  it 'inactive user is not active_for_authentication' do
-    new_user = User.new
-    new_user.active = false
-    expect(new_user.active_for_authentication?).to eq(false)
+  context 'active user' do
+    let(:active_user) { build(:user, active: true) }
+    describe '#status' do
+      specify { expect(active_user.status).to eq('active') }
+    end
+    describe '#active_for_authentication' do
+      specify { expect(active_user.active_for_authentication?).to eq(true) }
+    end
   end
 
-  it 'responds to status with active or inactive' do
-    new_user = User.new
-    new_user.active = false
-    expect(new_user.status).to eq('inactive')
-    new_user.active = true
-    expect(new_user.status).to eq('active')
+  context 'inactive user' do
+    let(:inactive_user) { build(:user, active: false) }
+    describe '#status' do
+      specify { expect(inactive_user.status).to eq('inactive') }
+    end
+    describe '#active_for_authentication' do
+      specify { expect(inactive_user.active_for_authentication?).to eq(false) }
+    end
   end
+
+  describe '#name_was' do
+    it 'retains the original full name during a name change' do
+      original_full_name = "#{user.first_name} #{user.last_name}"
+      user.first_name = 'CHANGED'
+      expect(original_full_name).to eq(user.name_was)
+    end
+  end
+
 end
