f82fe711f1002b392891d0a0572c45cc8b3a2b9f
Yong Joseph Bakos <ybakos@humanoriented.com>
2020-05-18 11:20:19 -0700

    Activity: Refactor model tests.
    References #29.

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -4,56 +4,61 @@ RSpec.describe User, type: :model do
   let(:activity) { create :activity }
 
   describe 'attributes' do
+    specify { expect(activity).to respond_to(:title) }
+    specify { expect(activity).to respond_to(:when_start) }
+    specify { expect(activity).to respond_to(:when_end) }
+    specify { expect(activity).to respond_to(:description) }
+    specify { expect(activity).to respond_to(:location) }
+    specify { expect(activity).to respond_to(:contact_name) }
+    specify { expect(activity).to respond_to(:contact_number) }
+    specify { expect(activity).to respond_to(:contact_email) }
+    specify { expect(activity).to respond_to(:state) }
+    specify { expect(activity).to respond_to(:ongoing) }
+    specify { expect(activity).to respond_to(:organization_name) }
     specify { expect(activity).to respond_to(:url) }
   end
 
   describe 'validations' do
+    specify { expect(activity).to validate_presence_of(:title) }
     it 'url format' do
       expect(activity).to allow_value('http://fake.com').for(:url)
       expect(activity).to allow_value('https://fake.com').for(:url)
       expect(activity).to allow_value(nil).for(:url)
       expect(activity).to_not allow_value('FAKE').for(:url)
     end
+    it 'ensure end date is not before start date' do
+      expect(activity).to be_valid
+      activity.when_end = activity.when_start - 1.day
+      expect(activity).to_not be_valid
+    end
   end
 
-  it 'has a required title' do
-    assert activity.valid?
-    activity.title = ''
-    refute activity.valid?
+  describe 'tags' do
+    specify { expect(activity).to respond_to(:tag_list) }
+
+    it 'converts tags to lowercase' do
+      activity.tag_list = 'Fake1, fAke2, FAKE3'
+      activity.save
+      expect(activity.tag_list).to eq(['fake1', 'fake2', 'fake3'])
+    end
+
+    it 'removes unused tags' do
+      @tags = ActsAsTaggableOn::Tag.all
+
+      activity.tag_list.add('taco')
+      activity.tag_list.add('burrito')
+      activity.tag_list.add('banana')
+      activity.tag_list.add('enchilada')
+      activity.save
+
+      assert_equal(@tags.count, 4)
+
+      activity.tag_list.remove('banana')
+      activity.save
+
+      expect(@tags.count).to eq(3)
+      refute @tags.any? {|tag| tag.name == 'banana' }
+    end
   end
 
-  it 'validates when_start before when_end' do
-    assert activity.valid?
-    activity.when_start = Time.now
-    activity.when_end = Time.now - 1.day
-    refute activity.valid?
-  end
-
-  it 'has a list of tags' do
-    assert activity.has_attribute? 'tag_list'
-  end
-
-  it 'converts tags to lowercase' do
-    activity.tag_list = 'fOOd, yUmmy, Bend'
-    activity.save
-    expect(activity.tag_list).to eq(['food', 'yummy', 'bend'])
-  end
-
-  it 'removes unused tags' do
-    @tags = ActsAsTaggableOn::Tag.all
-
-    activity.tag_list.add('taco')
-    activity.tag_list.add('burrito')
-    activity.tag_list.add('banana')
-    activity.tag_list.add('enchilada')
-    activity.save
-
-    assert_equal(@tags.count, 4)
-
-    activity.tag_list.remove('banana')
-    activity.save
-
-    expect(@tags.count).to eq(3)
-    refute @tags.any? {|tag| tag.name == 'banana' }
-  end
 end
