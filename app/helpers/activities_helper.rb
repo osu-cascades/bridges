e82d80a6e509f4e7dc63bba1856229e43d557e0d
Yong Bakos <ybakos@humanoriented.com>
2020-09-08 00:59:29 -0700

    Activity: Display day of week checkboxes in form.
    Refrences #52.

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -1,2 +1,14 @@
 module ActivitiesHelper
+
+  DAYS_OF_WEEK = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+
+  def day_checkboxes(form)
+    ''.tap do |result|
+      DAYS_OF_WEEK.each do |day|
+        result << form.check_box('days', {multiple: true, class: 'form-check-input'}, day.capitalize, '')
+        result << form.label(:"days_#{day}", day.capitalize, class: 'form-check-label mr-3')
+      end
+    end.html_safe
+  end
+
 end
---
+++ spec/helpers/activities_helper_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe ActivitiesHelper, type: :helper do
+
+  describe '#day_checkboxes' do
+    pending
+  end
+
+end
