3488643f643cd3df4a06fcad59cc91f982622f07
Yong Bakos <ybakos@humanoriented.com>
2021-11-15 14:49:34 -0800

    Config: Remove email domain.
    It is not needed, so let's remove it.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,6 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'heroku.com',
     user_name:            ENV['SMTP_USERNAME'],
     password:             ENV['SMTP_PASSWORD'],
     authentication:       'plain',
