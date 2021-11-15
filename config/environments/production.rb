18d70c9d58efb91ea7fcdcb79f529e31a9482fcf
Yong Bakos <ybakos@humanoriented.com>
2021-11-15 13:43:51 -0800

    Config: Replace sendgrid with gmail.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,11 +67,11 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'http://hdesd-bridges.herokuapp.com/' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-    address:              'smtp.sendgrid.net',
+    address:              'smtp.gmail.com',
     port:                 587,
     domain:               'heroku.com',
-    user_name:            ENV["SENDGRID_USERNAME"],
-    password:             ENV["SENDGRID_PASSWORD"],
+    user_name:            ENV['SMTP_USERNAME'],
+    password:             ENV['SMTP_PASSWORD'],
     authentication:       'plain',
     enable_starttls_auto: true
   }
